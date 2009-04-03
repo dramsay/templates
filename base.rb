510693b196895348f528c9cd93b8a157ecc5dc58
Doug Ramsay <doug@intridea.com>
2009-04-02 22:35:48 -0400

    Updating jquery version

--- base.rb
+++ base.rb
@@ -51,7 +51,7 @@ if yes?("Freeze Rails? (yes/no)")
 end
 
 run "rm -f public/javascripts/*"
-run "curl -L http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js > public/javascripts/jquery.js"
+run "curl -L http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js > public/javascripts/jquery.js"
 run "curl -L http://jqueryjs.googlecode.com/svn/trunk/plugins/form/jquery.form.js > public/javascripts/jquery.form.js"
 
 generate('rspec')
