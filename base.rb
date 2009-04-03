0cc3f70520fa9db3537e517609cc1e6dcdaf65c9
Doug Ramsay <doug@intridea.com>
2009-04-02 22:55:53 -0400

    Making some changes to how base JS is removed

--- base.rb
+++ base.rb
@@ -50,7 +50,7 @@ if yes?("Freeze Rails? (yes/no)")
   freeze!
 end
 
-run "rm -f public/javascripts/*"
+run "git rm public/javascripts/controls.js public/javascripts/dragdrop.js public/javascripts/effects.js public/javascripts/prototype.js"
 run "curl -L http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js > public/javascripts/jquery.js"
 run "curl -L http://jqueryjs.googlecode.com/svn/trunk/plugins/form/jquery.form.js > public/javascripts/jquery.form.js"
 
